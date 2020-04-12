0493705595cbf844bfcd40191cd8870cc468157f
jgm0710 <62986636+jgm0710@users.noreply.github.com>
2020-04-12 22:31:58 +0900

    BABBA

---
+++ 9000/9600/9625.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#pragma warning (disable:4996)
+using namespace std;
+// 주석
+int main(){
+	int k;
+	int a = 1, b = 0;
+	cin >> k;
+	while (k--) {
+		int tmp = a;
+		a = b;
+		b = b + tmp;
+	}
+
+	cout << a << " " << b;
+}
