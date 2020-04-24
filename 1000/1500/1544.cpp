7291eb250ef908e5369e134167c051acbd8a8503
jgm0710 <62986636+jgm0710@users.noreply.github.com>
2020-04-25 03:04:50 +0900

    사이클 단어(맞왜틀?)

--- 1000/1500/1544.cpp
+++ 1000/1500/1544.cpp
@@ -2,54 +2,41 @@
 #include<string>
 using namespace std;
 //사이클 단어
-//거의 다왔는데 너무 오래 붙잡고 있어서 일단 포기
+//a.find("tmp") == string::npos 를 사용해서 이긴다
 
-//단어를 돌리기 위한 함수
-string turnWord(int n, string a) {
-	string tmp;
-	for (int i = 0; i < a.length(); i++) {
-		if (a[n] == 0) {
-			n = 0;
-		}
-		tmp += a[n];
-		n++;
-	}
-	return tmp;
-}
 
-//같은 문자인지를 비교하는 함수
-bool stringEquals(string a, string b) {
-	if (a == b)
-		return true;
-	return false;
-}
-
-string word[50];
+string word[100];
 int main(){
 	int n;
 	cin >> n;
-	int count = 0;
-	for (int i = 0; i < n; i++) {	//n번 단어를 입력 받음 
+
+	int count=0;
+
+	int i = 0;
+	while (n--) {
 		string a;
 		cin >> a;
 
-		if (i == 0) {
-			word[i] = a;
-			continue;
-		}
+		bool add_flag = true;
 
-		for (int j = 0; j < i; j++) {	//새로 들어오는 a와 기존 word내의 내용을 비교
-			bool cnt_flag = false;
-			for (int k = 0; k < a.length(); k++) {
-				cnt_flag = stringEquals(turnWord(k, word[j]), a);
-				if (cnt_flag) {
-					cout <<"출력 : "<< turnWord(k, word[j]) << " " << a << endl;
-					break;
-				}
+		for (int j = 0; word[j][0] != 0; j++) {
+			string tmp;
+			tmp = word[j];
+			tmp += word[j];
+
+			if (tmp.find(a) != string::npos) {		//word에 a가 포함되어있으면
+				add_flag = false;
+				break;
 			}
-			if (!cnt_flag)
-				count++;
+		}
+		if (add_flag) {
+			word[i++] = a;
+			count++;
 		}
 	}
+
+	/*for (i = 0; word[i][0] != 0; i++) {
+		cout << word[i] << endl;
+	}*/
 	cout << count;
 }
