b510e05e3b24877370fa6affa7d06e5f0e721393
jgm0710 <62986636+jgm0710@users.noreply.github.com>
2020-05-01 18:47:52 +0900

    박스 (일단 실패)

---
+++ 9000/9400/9455.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+//박스
+
+int grid[110][110];
+int main(){
+	int t;
+
+	cin >> t;
+
+	
+
+	while (t--) {
+		int m, n; //m행 n열
+
+		cin >> m >> n;
+
+		int count = 0;
+
+		for (int i = m-1; i >=0; i--) {
+			for (int j = n-1; j >=0 ; j--) {
+				cin >> grid[i][j];
+			}
+		}
+		//cout << endl;
+
+		for (int j = 0; j < n; j++) {
+			for (int i = 0; i < m; i++) {
+				if (grid[i][j] == 1) {
+					continue;
+				}
+				else if (grid[i][j] == 0) {
+					int tmp = 0;
+					for (int k = 0; k < n; k++) {
+						if (grid[i + tmp][j] == 1) {
+							grid[i][j] = 1;
+							grid[i + tmp][j] = 0;
+							count += tmp;
+							break;
+						}
+						else if (grid[i + tmp][j] == 0) {
+							tmp++;
+						}
+					}
+				}
+			}
+		}
+		cout << endl;
+
+		for (int i = m - 1; i >= 0; i--) {
+			for (int j = n - 1; j >= 0; j--) {
+				cout<< grid[i][j]<<" ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+
+		cout << count << endl;
+
+		/*for (int i = 0; i < 110; i++) {
+			for (int j = 0; j < 110; j++) {
+				grid[i][i] = 0;
+			}
+		}*/
+	}
+
+}
