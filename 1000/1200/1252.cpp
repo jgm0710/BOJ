2d21e7021481fd6ad34760ddb34f628a1d7bca4b
jgm0710 <62986636+jgm0710@users.noreply.github.com>
2020-05-02 15:51:39 +0900

    이진수 덧셈

--- 1000/1200/1252.cpp
+++ 1000/1200/1252.cpp
@@ -5,50 +5,48 @@ using namespace std;
 //이진수 덧셈
 //80 자리 수 이기 때문에 string으로 처리
 
-int bn1[90];
-int bn2[90];
-int result[90];
-
-int max(int x, int y) {
-	if (x > y)
-		return x;
-	else
-		return y;
-}
+int bn1[100];
+int bn2[100];
+int result[100];
 
 int main(){
-
 	string a, b;
-
 	cin >> a >> b;
 
-	int result_length=max(a.length(), b.length());
-	
 
 	reverse(a.begin(), a.end());
 	reverse(b.begin(), b.end());
 
-	for (int i = 0; a[i] != 0; i++)
+	for (int i = 0; i < a.length(); i++) {
 		bn1[i] = a[i] - '0';
-	for (int i = 0; b[i] != 0; i++)
+	}
+	for (int i = 0; i < b.length(); i++) {
 		bn2[i] = b[i] - '0';
-	
-	for (int i = 0; i < 90; i++) {
+	}
+
+	for (int i = 0; i < 100; i++) {
 		result[i] = bn1[i] + bn2[i];
+	}
+
+	//자리수 올림
+	for (int i = 0; i < 100; i++) {
 		if (result[i] > 1) {
-			result[i] -= 2;
+			result[i] %= 2;
 			result[i + 1]++;
 		}
 	}
-	
-	if (result[result_length] == 1) {
-		for (int i = result_length; i >= 0; i--) {
-			cout << result[i]<<"up";
-		}
+
+	int tmp_i = 0;
+	for (int i = 99;  result[i]==0&&i>0; i--) {
+		tmp_i = i;
+	}
+	if (tmp_i == 0) {
+		cout << 0;
 	}
 	else {
-		for (int i = result_length-1; i >= 0; i--) {
+		for (int i = tmp_i - 1; i >= 0; i--) {
 			cout << result[i];
 		}
 	}
+	
 }
