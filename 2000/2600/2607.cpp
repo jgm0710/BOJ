4795fd90ed7323e935d860a867a6a0df71eec53d
jgm0710 <62986636+jgm0710@users.noreply.github.com>
2020-05-02 16:22:00 +0900

    비슷한 단어

--- 2000/2600/2607.cpp
+++ 2000/2600/2607.cpp
@@ -1,75 +1,52 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cstdlib>
 using namespace std;
 //비슷한 단어
 
-string st[105];
+int a_alphabet[26];
+int b_alphabet[26];
 
 int main(){
 	int n;
-	int count = 0;
-	string a,b;
 	cin >> n;
+	string a,b;
 
-
+	int count = 0;
 
 	for (int i = 0; i < n; i++) {
+		int difference = 0;
+
 		if (i == 0) {
 			cin >> a;
-			continue;
-		}
-		cin >> b;
-
-		//사전 순으로 정렬해서 문자 비교
-		//같은 문자로만 이루어진 단어인지를 알기 위해
-		string t_a = a;
-		string t_b = b;
-		sort(t_a.begin(), t_a.end());
-		sort(t_b.begin(), t_b.end());
-
-		if (t_a == t_b) {
-			count++;
-			cout << t_a << " " << t_b<<" "<<count << endl;
-			continue;
-		}
-
-		//a 에서 문자를 하나 빼서 b와 비교
-		string tmp; //a에서 문자를 하나 빼서 담을 string
-		bool continue_flag = false;
-		for (int i = 0; i < a.length(); i++) {
 			for (int j = 0; j < a.length(); j++) {
-				if (i == j)
-					continue;
-				tmp += a[i];
+				a_alphabet[a[j] - 'A']++;
 			}
-			cout << tmp << " " << b << endl;
-			if (tmp == b) {
-				count++;
-				continue_flag = true;
-			}
-		}
-		if (continue_flag)
 			continue;
+		}
 
-		//b 에서 문자를 하나 빼서 a와 비교
-		tmp="";
-		continue_flag = false;
-		for (int i = 0; i < b.length(); i++) {
-			for (int j = 0; j < b.length(); j++) {
-				if (i == j)
-					continue;
-				tmp += b[i];
-			}
-			if (tmp == a) {
+		cin >> b;
+		for (int j = 0; j < b.length(); j++) {
+			b_alphabet[b[j] - 'A']++;
+		}
+
+		for (int j = 0; j < 26; j++) {
+			difference += abs(a_alphabet[j] - b_alphabet[j]);
+		}
+
+		if (difference < 2) {
+			count++;
+		}
+		else if (difference == 2) {
+			if (a.length() == b.length()) {
 				count++;
-				continue_flag = true;
 			}
 		}
-		if (continue_flag)
-			continue;
+		for (int i = 0; i < 26; i++) {
+			b_alphabet[i] = 0;
+		}
 	}
-
 	cout << count;
 
 }
