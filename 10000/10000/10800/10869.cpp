d039265155ba4f385db623e84c4c063a360f3a7f
jgm0710 <62986636+jgm0710@users.noreply.github.com>
2020-05-09 23:37:03 +0900

    사칙연산

---
+++ 10000/10000/10800/10869.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//사칙연산
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b;
+}
