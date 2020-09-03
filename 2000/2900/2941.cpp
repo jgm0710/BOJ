92c161af81beeb2375fbe8866c37b273900db4a5
jgm0710 <62986636+jgm0710@users.noreply.github.com>
2020-09-03 19:07:00 +0900

    2941 크로아티아 알파벳

---
+++ 2000/2900/2941.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+//2941 크로아티아 알파벳
+
+string a;
+
+int count;
+
+int spot;
+
+void CroatiaCount() {
+	string b;
+
+	
+}
+
+int main(){
+
+	cin >> a;
+
+
+	if (a == "aa")
+		cout << "true";
+
+}
